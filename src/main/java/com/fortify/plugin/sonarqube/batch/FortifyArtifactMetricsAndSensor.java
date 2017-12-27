3f3665ef147a8a07fc692d54ab43baebcf04a5ca
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2017-12-27 20:12:45 +0100

    Fix related to loading artifact information

--- src/main/java/com/fortify/plugin/sonarqube/batch/FortifyArtifactMetricsAndSensor.java
+++ src/main/java/com/fortify/plugin/sonarqube/batch/FortifyArtifactMetricsAndSensor.java
@@ -114,7 +114,7 @@ public final class FortifyArtifactMetricsAndSensor extends AbstractFortifyMetric
 			return connFactory.getConnectionWithArtifactProcessing().api().artifact().queryArtifacts(connFactory.getApplicationVersionId())
 				.paramOrderBy("uploadDate", SSCOrderByDirection.DESC)
 				.paramEmbedScans()
-				.preProcessor(new JSONMapFilterSpEL(MatchMode.INCLUDE, "(_embed.scans?.getJSONObject(0)?.type=='SCA' && status=='PROCESS_COMPLETE') || status matches 'PROCESSING|SCHED_PROCESSING|REQUIRE_AUTH|ERROR_PROCESSING'"))
+				.preProcessor(new JSONMapFilterSpEL(MatchMode.INCLUDE, "(_embed.scans?.get(0)?.type=='SCA' && status=='PROCESS_COMPLETE') || status matches 'PROCESSING|SCHED_PROCESSING|REQUIRE_AUTH|ERROR_PROCESSING'"))
 				.useCache(true).maxResults(1).build().getUnique();
 		}
 	}
